d54e0c705bbb359bf25a9ac0cc4ca22e04d91ce9
YolandaYang <yangkecoy@gmail.com>
2016-08-30 14:00:33 -0500

    Update appIE.js

--- js/appIE.js
+++ js/appIE.js
@@ -361,7 +361,7 @@ function ducklingtip(e){
   		ducklingwords++;
   		break;
  case 3:
-  		tip('<div class="speakbubble">When I grow up I can lay eggs! Kwak!</div>',e);
+  		tip('<div class="speakbubble"><a href="http://www.narrativecard.com/aboutme/draw_app.html">If you are bored, try this app</a></div>',e);
   		ducklingwords++;
   		break;
   default:
